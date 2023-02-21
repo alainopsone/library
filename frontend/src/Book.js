bcfb5518a0615e23e2de881c56bf3c46e89a044d
Alain TRAN <alain.tran@opsone.net>
2023-02-21 12:35:55 +0100

    remove string (added in HTML)

--- frontend/src/Book.js
+++ frontend/src/Book.js
@@ -3,28 +3,35 @@ export default class Book {
   constructor(infos) {
     this.infos = {
       id: `book-${Date.now()}${Math.floor(Math.random() * 100)}`
+      // ...infos
     }
 
     Object.entries(infos).forEach(([key, value]) => {
       this.infos[key] = value
     })
+
+    // this.id = `book-${Date.now()}${Math.floor(Math.random() * 100)}`
+    // this.title = infos.title
+    // this.author = infos.author
+    // this.page = infos.page
+    // this.read = infos.read
   }
 
   get cardElement() { return document.getElementById(this.infos.id) }
 
   set title(value) {
-    this.cardElement.querySelector('[data-book-title]').textContent = `Title: ${value || '...'}`
+    this.cardElement.querySelector('[data-book-title]').textContent = `${value || '...'}`
   }
 
   set author(value) {
-    this.cardElement.querySelector('[data-book-author]').textContent = `Author: ${value || '...'}`
+    this.cardElement.querySelector('[data-book-author]').textContent = `${value || '...'}`
   }
 
   set page(value) {
-    this.cardElement.querySelector('[data-book-page]').textContent = `Page: ${value || '...'}`
+    this.cardElement.querySelector('[data-book-page]').textContent = `${value || '...'}`
   }
 
   set read(value) {
-    this.cardElement.querySelector('[data-book-read]').textContent = `Read: ${value ? 'Yes' : 'No'}`
+    this.cardElement.querySelector('[data-book-read]').textContent = `${value ? 'Yes' : 'No'}`
   }
 }
