b51407953d5e22b988c35d69ea54476ca4e6b07d
Alain TRAN <alain.tran@opsone.net>
2023-02-22 16:35:21 +0100

    clean file

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -1,17 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
     extend: {
       backgroundImage: {
         'invalid-input': "url('/invalid-red.webp')",
-        'valid-input': "url('/checked-green.webp')",
+        'valid-input': "url('/checked-green.webp')"
       },
       keyframes: {
-        'shake': {
+        shake: {
           '0%, 100%': {
             transform: 'translateX(0)'
           },
@@ -42,122 +42,123 @@ module.exports = {
         },
         'flicker-out': {
           '0%': {
-            opacity: '1',
+            opacity: '1'
           },
           '13.9%': {
-            opacity: '1',
+            opacity: '1'
           },
           '14%': {
             opacity: '0',
-            'box-shadow': 'none',
+            'box-shadow': 'none'
           },
           '14.9%': {
             opacity: '0',
-            'box-shadow': 'none',
+            'box-shadow': 'none'
           },
           '15%': {
-            opacity: '1',
+            opacity: '1'
           },
           '22.9%': {
-            opacity: '1',
+            opacity: '1'
           },
           '23%': {
             opacity: '0',
-            'box-shadow': 'none',
+            'box-shadow': 'none'
           },
           '24.9%': {
             opacity: '0',
-            'box-shadow': 'none',
+            'box-shadow': 'none'
           },
           '25%': {
-            opacity: '1',
+            opacity: '1'
           },
           '34.9%': {
-            opacity: '1',
+            opacity: '1'
           },
           '35%': {
             opacity: '0',
-            'box-shadow': 'none',
+            'box-shadow': 'none'
           },
           '39.9%': {
             opacity: '0',
-            'box-shadow': 'none',
+            'box-shadow': 'none'
           },
           '40%': {
-            opacity: '1',
+            opacity: '1'
           },
           '42.9%': {
-            opacity: '1',
+            opacity: '1'
           },
           '43%': {
             opacity: '0',
-            'box-shadow': 'none',
+            'box-shadow': 'none'
           },
           '44.9%': {
             opacity: '0',
-            'box-shadow': 'none',
+            'box-shadow': 'none'
           },
           '45%': {
-            opacity: '1',
+            opacity: '1'
           },
           '50%': {
-            opacity: '1',
+            opacity: '1'
           },
           '54.9%': {
-            opacity: '1',
+            opacity: '1'
           },
           '55%': {
             opacity: '0',
-            'box-shadow': 'none',
+            'box-shadow': 'none'
           },
           '69.4%': {
             opacity: '0',
-            'box-shadow': 'none',
+            'box-shadow': 'none'
           },
           '69.5%': {
-            opacity: '1',
+            opacity: '1'
           },
           '69.9%': {
-            opacity: '1',
+            opacity: '1'
           },
           '70%': {
             opacity: '0',
-            'box-shadow': 'none',
+            'box-shadow': 'none'
           },
           '79.4%': {
             opacity: '0',
-            'box-shadow': 'none',
+            'box-shadow': 'none'
           },
           '79.9%': {
-            opacity: '1',
+            opacity: '1'
           },
           '80%': {
             opacity: '0',
-            'box-shadow': 'none',
+            'box-shadow': 'none'
           },
           '89.8%': {
             opacity: '0',
-            'box-shadow': 'none',
+            'box-shadow': 'none'
           },
           '89.9%': {
             opacity: '1',
-            'box-shadow': 'none',
+            'box-shadow': 'none'
           },
           '90%': {
             opacity: '0',
-            'box-shadow': 'none',
+            'box-shadow': 'none'
           },
           '100%': {
-            opacity: '0',
-          },
+            opacity: '0'
+          }
         }
       },
       animation: {
         'swing-in': 'swing-in 0.5s cubic-bezier(0.175, 0.885, 0.320, 1.275) both',
+        'swing-in-right-fwd': '2s cubic-bezier(0.175, 0.885, 0.320, 1.275) both',
         'flicker-out': 'flicker-out 2s linear both',
-        'shake': 'shake 0.8s cubic-bezier(0.455, 0.030, 0.515, 0.955) both',
+        shake: 'shake 0.8s cubic-bezier(0.455, 0.030, 0.515, 0.955) both'
       }
-    },
+    }
   },
-  plugins: [],
+  plugins: []
 }
